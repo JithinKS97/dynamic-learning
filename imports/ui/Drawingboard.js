998b0f75d696a8c40d7cc1a2ba1ae532d1b950cb
jithunni.ks@gmail.com <jithunni.ks@gmail.com>
2018-05-05 09:47:07 +0530

    drawing board component created

--- imports/ui/Drawingboard.js
+++ imports/ui/Drawingboard.js
@@ -1,16 +1,35 @@
 import React from 'react'
+import './drawing-board/drawingboard';
+import './drawing-board/drawingboard.css';
+
+
+
 
 export default class Drawingboard extends React.Component {
+
+    componentDidMount() {
+        this.myBoard = new DrawingBoard.Board('board', {
+            background: "#000000",
+            color: "#ffffff",
+            size: 5,
+            controls: ['Color',
+              { DrawingMode: { filler: false } },
+              { Size: { type: "range" } },
+              'Navigation',
+            ],
+            webStorage: 'session'
+          });
+    }
+
     render() {
 
         const boardStyle = {
             width:400,
             height:400,
-            backgroundColor:'red'
         }
 
         return(
-        <div style={boardStyle}>
+        <div id='board' style={boardStyle}>
             <p>Drawing-board</p>
         </div>
         );
