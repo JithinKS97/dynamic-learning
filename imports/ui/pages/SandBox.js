03bd129f73fb46bdb8b07d1367d4d2530f9b4636
jithunni.ks@gmail.com <jithunni.ks@gmail.com>
2018-06-06 11:18:26 +0530

    removed immutable js

--- imports/ui/pages/SandBox.js
+++ imports/ui/pages/SandBox.js
@@ -9,13 +9,14 @@ export default class Sandbox extends React.Component {
 
     this.state = {
       isDrawing:false,
-      ppts: new Immutable.List()
     }
+
   }
 
   componentDidMount() {
 
     this.mouse = {x:0, y:0}
+    this.prev_mouse = {x: 0, y: 0}
 
     const container_style = getComputedStyle(this.container)
 
@@ -29,8 +30,8 @@ export default class Sandbox extends React.Component {
     this.ctx.lineJoin = 'round'
     this.ctx.lineCap = 'round'
     this.ctx.strokeStyle = 'white'
-    this.ctx.shadowBlur = this.ctx.lineWidth*1.1
-    this.ctx.shadowColor = this.ctx.strokeStyle
+    this.ctx.shadowBlur = this.ctx.lineWidth*1.25
+    this.ctx.shadowColor = 'white'
 
     this.pts = []
 
@@ -47,11 +48,8 @@ export default class Sandbox extends React.Component {
 
   onMouseUp() {
 
-    this.setState(prevState => {
-      return {
-        ppts: prevState.ppts.clear(),
-        isDrawing:false
-      }
+    this.setState({
+      isDrawing: false
     })
 
     this.pts = []
@@ -62,20 +60,10 @@ export default class Sandbox extends React.Component {
     if(this.state.isDrawing) {
 
       const boundingRect = this.container.getBoundingClientRect();
+
       
       this.mouse.x = mouseEvent.clientX - boundingRect.left
       this.mouse.y = mouseEvent.clientY - boundingRect.top
-
-      const point = new Immutable.Map({
-        x: mouseEvent.clientX - boundingRect.left,
-        y: mouseEvent.clientY - boundingRect.top,
-      })
-
-      this.setState(prevState => {
-        return {
-          ppts: prevState.ppts.push(point),
-        }
-      })
 
       this.pts.push({x:this.mouse.x, y:this.mouse.y})
 
@@ -85,45 +73,24 @@ export default class Sandbox extends React.Component {
 
   draw() {
 
-    const { ppts } = this.state
-
     this.ctx.beginPath()
-    const pts = ppts.get(0)
 
-    if(pts) {
-      this.ctx.moveTo(pts.get('x'), pts.get('y'))
-    }
+    this.ctx.moveTo(this.pts[0].x, this.pts[0].y);
 
-    ppts.map((pts, index) => {
+    this.pts.map( (pt, index) => {
+      
+      if(index>0 && index<this.pts.length-2) {
 
-      if(index>0 && index<ppts.size-2) {
-
-        const next_pts = ppts.get(index)
-
-        const c = (pts.get('x') + next_pts.get('x')) / 2
-        const d = (pts.get('y') + next_pts.get('y')) / 2
-  
-        this.ctx.quadraticCurveTo(pts.get('x'), pts.get('y'), c, d)
+        let c = (this.pts[index].x + this.pts[index + 1].x) / 2
+        let d = (this.pts[index].y + this.pts[index + 1].y) / 2
     
+        this.ctx.quadraticCurveTo(this.pts[index].x, this.pts[index].y, c, d)
+      
       }
-
+      else if(index == this.pts.length-2) {
+        this.ctx.quadraticCurveTo(this.pts[index].x, this.pts[index].y, this.pts[index+1].x, this.pts[index+1].y)
+      }
     })
-
-
-    // this.pts.map( (pt, index) => {
-      
-    //   if(index>0 && index<this.pts.length-2) {
-
-    //     let c = (this.pts[index].x + this.pts[index + 1].x) / 2
-    //     let d = (this.pts[index].y + this.pts[index + 1].y) / 2
-    
-    //     this.ctx.quadraticCurveTo(this.pts[index].x, this.pts[index].y, c, d)
-      
-    //   }
-    //   else if(index == this.pts.length-2)
-    //     this.ctx.quadraticCurveTo(this.pts[index].x, this.pts[index].y, this.pts[index+1].x, this.pts[index+1].y)
-
-    // })
 
     this.ctx.stroke()
   }
@@ -149,3 +116,109 @@ export default class Sandbox extends React.Component {
     )
   }
 }
+
+// (function() {
+	
+// 	var canvas = document.querySelector('#paint');
+// 	var ctx = canvas.getContext('2d');
+	
+// 	var sketch = document.querySelector('#sketch');
+// 	var sketch_style = getComputedStyle(sketch);
+// 	canvas.width = parseInt(sketch_style.getPropertyValue('width'));
+// 	canvas.height = parseInt(sketch_style.getPropertyValue('height'));
+	
+	
+// 	// Creating a tmp canvas
+// 	var tmp_canvas = document.createElement('canvas');
+// 	var tmp_ctx = tmp_canvas.getContext('2d');
+// 	tmp_canvas.id = 'tmp_canvas';
+// 	tmp_canvas.width = canvas.width;
+// 	tmp_canvas.height = canvas.height;
+	
+// 	sketch.appendChild(tmp_canvas);
+
+// 	var mouse = {x: 0, y: 0};
+// 	var last_mouse = {x: 0, y: 0};
+	
+// 	// Pencil Points
+// 	var ppts = [];
+	
+// 	/* Mouse Capturing Work */
+// 	tmp_canvas.addEventListener('mousemove', function(e) {
+// 		mouse.x = typeof e.offsetX !== 'undefined' ? e.offsetX : e.layerX;
+// 		mouse.y = typeof e.offsetY !== 'undefined' ? e.offsetY : e.layerY;
+// 	}, false);
+	
+	
+// 	/* Drawing on Paint App */
+// 	tmp_ctx.lineWidth = 5;
+// 	tmp_ctx.lineJoin = 'round';
+// 	tmp_ctx.lineCap = 'round';
+// 	tmp_ctx.strokeStyle = 'blue';
+// 	tmp_ctx.fillStyle = 'blue';
+	
+// 	tmp_canvas.addEventListener('mousedown', function(e) {
+// 		tmp_canvas.addEventListener('mousemove', onPaint, false);
+		
+// 		mouse.x = typeof e.offsetX !== 'undefined' ? e.offsetX : e.layerX;
+// 		mouse.y = typeof e.offsetY !== 'undefined' ? e.offsetY : e.layerY;
+		
+// 		ppts.push({x: mouse.x, y: mouse.y});
+		
+// 		onPaint();
+// 	}, false);
+	
+// 	tmp_canvas.addEventListener('mouseup', function() {
+// 		tmp_canvas.removeEventListener('mousemove', onPaint, false);
+		
+// 		// Writing down to real canvas now
+// 		ctx.drawImage(tmp_canvas, 0, 0);
+// 		// Clearing tmp canvas
+// 		tmp_ctx.clearRect(0, 0, tmp_canvas.width, tmp_canvas.height);
+		
+// 		// Emptying up Pencil Points
+// 		ppts = [];
+// 	}, false);
+	
+// 	var onPaint = function() {
+		
+// 		// Saving all the points in an array
+// 		ppts.push({x: mouse.x, y: mouse.y});
+		
+// 		if (ppts.length < 3) {
+// 			var b = ppts[0];
+// 			tmp_ctx.beginPath();
+// 			//ctx.moveTo(b.x, b.y);
+// 			//ctx.lineTo(b.x+50, b.y+50);
+// 			tmp_ctx.arc(b.x, b.y, tmp_ctx.lineWidth / 2, 0, Math.PI * 2, !0);
+// 			tmp_ctx.fill();
+// 			tmp_ctx.closePath();
+			
+// 			return;
+// 		}
+		
+// 		// Tmp canvas is always cleared up before drawing.
+// 		tmp_ctx.clearRect(0, 0, tmp_canvas.width, tmp_canvas.height);
+		
+// 		tmp_ctx.beginPath();
+// 		tmp_ctx.moveTo(ppts[0].x, ppts[0].y);
+		
+// 		for (var i = 1; i < ppts.length - 2; i++) {
+// 			var c = (ppts[i].x + ppts[i + 1].x) / 2;
+// 			var d = (ppts[i].y + ppts[i + 1].y) / 2;
+			
+// 			tmp_ctx.quadraticCurveTo(ppts[i].x, ppts[i].y, c, d);
+// 		}
+		
+// 		// For the last 2 points
+// 		tmp_ctx.quadraticCurveTo(
+// 			ppts[i].x,
+// 			ppts[i].y,
+// 			ppts[i + 1].x,
+// 			ppts[i + 1].y
+// 		);
+// 		tmp_ctx.stroke();
+		
+// 	};
+	
+// }());
