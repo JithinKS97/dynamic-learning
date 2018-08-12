b4658ba86a069f83bd8aab5e085c3c82ca5d3349
JithinKS97 <jithunni.ks@gmail.com>
2018-08-12 16:27:40 +0530

    lessonplan viewer styles

--- imports/ui/components/LessonPlanViewer.js
+++ imports/ui/components/LessonPlanViewer.js
@@ -264,7 +264,7 @@ class LessonPlanViewer extends React.Component {
 
         return (
             
-        <Segment style = {{padding:0, margin:0}}>
+        <Segment style = {{padding:'0 0.8rem', margin:0}}>
 
             <Dimmer active = {!this.state.initialized}>
                 <Loader />
