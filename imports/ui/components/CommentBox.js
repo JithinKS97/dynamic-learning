251e911dccfbc711090a2a9649fd9ad28d265dcd
JithinKS97 <jithunni.ks@gmail.com>
2018-08-19 23:15:52 +0530

    Comments requests

--- imports/ui/components/CommentBox.js
+++ imports/ui/components/CommentBox.js
@@ -32,21 +32,28 @@ export default class CommentBox extends React.Component {
         return (
             <Comment style = {{padding:'0.8rem', backgroundColor:'#eeeeee'}}>
                 <Comment.Content style = {{width:'100%'}}>
+                    {/* <Comment.Avatar src='/images/avatar/small/matt.jpg' /> */}
+                    {this.props.comment.userId == Meteor.userId()?<Button style = {{float:'right', padding:'0.5rem'}} onClick = {() =>{
 
-                        {this.props.comment.userId == Meteor.userId()?<Button style = {{float:'right', padding:'0.5rem'}} onClick = {() =>{
-
-                            const confirmation = confirm('Are you sure you want to delete your comment?')
-                            if(confirmation == true)
-                                this.props.deleteComment(this.props.index)
-                            }
-                        }>X</Button>:null}
-
-                    <p>{this.props.comment.comment} </p>
-                    <p>{this.momentNow.fromNow()}</p> 
-                    <p>{this.state.username}</p>                              
-                    
+                    const confirmation = confirm('Are you sure you want to delete your comment?')
+                    if(confirmation == true)
+                        this.props.deleteComment(this.props.index)
+                    }
+                    }>X</Button>:null}
+                    <div style = {{display:'flex', flexDirection:'row'}}>
+                        <Comment.Author>{this.state.username}</Comment.Author>
+                        <Comment.Metadata>
+                            <div>{this.momentNow.fromNow()}</div>
+                        </Comment.Metadata>
+                    </div>
+                    <div></div>
+                    <Comment.Text style = {{paddingTop:'0.8rem'}}>{this.props.comment.comment}</Comment.Text>
+                    {/* <Comment.Actions>
+                    <Comment.Action>Reply</Comment.Action>
+                    </Comment.Actions> */}
                 </Comment.Content>
-            </Comment>        
+            </Comment>
+                 
         )
     }
 }
