39ebdf55307f6e154a570766afbec04784711484
JithinKS97 <jithunni.ks@gmail.com>
2019-05-19 08:23:50 +0530

    object selection bug fixed

--- imports/ui/components/DrawingBoardCmp.js
+++ imports/ui/components/DrawingBoardCmp.js
@@ -205,7 +205,8 @@ export default class DrawingBoardCmp extends React.Component {
       this.b.loadFromJSON(canvasObjects);
     }
 
-    this.setSelectionStatus(false)
+    if(this.state.option != 'select')
+      this.setSelectionStatus(false)
   }
 
   setSelectionStatus = (status) => {
@@ -216,6 +217,7 @@ export default class DrawingBoardCmp extends React.Component {
       
       object.selectable = status; 
       object.hoverCursor = status?'hand':'default'
+
     });
   }
 
