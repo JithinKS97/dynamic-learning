bd262f4fa6bc6a4ad295079dbf0d0e3cf6533b8f
Nick McIntyre <nick@mcintyre.io>
2019-05-25 18:42:12 -0500

    Lint main.js

--- client/main.js
+++ client/main.js
@@ -1,15 +1,13 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import {Meteor} from 'meteor/meteor';
-import {AppRouter, onAuthChange} from '../imports/routes/AppRouter';
-import {LessonPlans} from '../imports/api/lessonplans'
-import '../imports/startup/simple-schema-configuration.js'
+import { Meteor } from 'meteor/meteor';
+import { AppRouter, onAuthChange } from '../imports/routes/AppRouter';
+import '../imports/startup/simple-schema-configuration';
 
-Tracker.autorun(()=>{
-  const isAuthenticated = !!Meteor.userId()
-  onAuthChange(isAuthenticated)
-})
+Tracker.autorun(() => {
+  const isAuthenticated = !!Meteor.userId();
+  onAuthChange(isAuthenticated);
+});
 
-Meteor.startup(()=>{
+Meteor.startup(() => {
   ReactDOM.render(AppRouter, document.getElementById('app'));
 });
