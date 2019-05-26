e929b5d0c21e2ebda77d206df0ba2bc7ff92e94e
Nick McIntyre <nick@mcintyre.io>
2019-05-26 10:53:53 -0500

    Lint imports/functions

--- imports/functions/index.js
+++ imports/functions/index.js
@@ -1,3 +1,4 @@
-export const generateSrc = (username, project_id) => {
-    return `https://editor.p5js.org/${username}/embed/${project_id}`;
-}
+/* eslint-disable import/prefer-default-export, camelcase */
+export const generateSrc = (username, project_id) => (
+  `https://editor.p5js.org/${username}/embed/${project_id}`
+);
