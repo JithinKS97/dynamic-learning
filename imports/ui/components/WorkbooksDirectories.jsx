541bb0f557b8634cf6f4ffc61d213714cdc16bcc
JithinKS97 <jithinks112@gmail.com>
2019-09-13 23:35:51 +0530

    some more fix workbook viewer

--- imports/ui/components/WorkbooksDirectories.jsx
+++ imports/ui/components/WorkbooksDirectories.jsx
@@ -279,7 +279,7 @@ class WorkbooksDirectories extends Component {
         <Modal
           size="fullscreen"
           open={!!selectedWorkbookId}
-          style={{ marginTop: '5rem', transform:'scale(0.7)' }}
+          style={{ transform:'scale(0.7)' }}
         >
           <Modal.Header>
             Preview
