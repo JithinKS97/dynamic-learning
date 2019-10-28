2d2c3885d7ba8c369ee1efecea73a6729d57b18b
JithinKS97 <jithinks112@gmail.com>
2019-10-28 10:09:03 +0530

    new load page

--- imports/ui/pages/LoadScreen.jsx
+++ imports/ui/pages/LoadScreen.jsx
@@ -1,31 +1,95 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from 'semantic-ui-react'
+import { useState } from 'react'
 
-export default class LoadScreen extends React.Component {
-  renderVideo = () => {
-    if (window.innerWidth > 700) {
-      return <video src="/video.mp4" autoPlay loop muted />;
-    }
-  }
+export default () => {
 
-  render() {
+  const Header = () => {
+
+    const [fontColor, setFontColor] = useState('white')
+
     return (
-      <header className="v-header container">
-        <div className="fullscreen-video-wrap">
-          {this.renderVideo()}
+      <div style={headerContainer}>
+        <div 
+          onMouseEnter={()=>setFontColor('black')} style={{
+            color: fontColor,
+            float:'right',
+            display:'inline-block',
+            fontSize:'1.5rem',
+            cursor:'pointer'
+          }}
+          onMouseLeave={()=>setFontColor('white')}
+        >
+          About
         </div>
-        <div className="header-overlay" />
-        <div className="header-content text-md-center">
-          <h1>Dynamic Learning</h1>
-          <p>
-            A platform where teachers and creative coders can collaborate to
-            create STEM lessons taught with the help of interactive simulations and animations.
-          </p>
-          <Link style={{ marginRight: '2.4rem' }} className="btn" to="/about">About</Link>
-          <Link className="btn" to="/explore">Explore</Link>
-          <Link style={{ marginLeft: '2.4rem' }} className="btn" to="/login">Log in</Link>
-        </div>
-      </header>
-    );
+      </div>
+    )
   }
+
+  const Buttons = () => {
+
+    const buttonContainerStyle = {
+      display:'flex',
+      flexDirection:'row',
+      justifyContent:'space-between',
+      marginTop:'3rem',
+    }
+
+    const buttonStyle = {
+      width:'10rem',
+      borderRadius:'20px',
+      height:'3rem',
+      fontSize:'1.2rem'
+    }
+
+    return (
+      <div style={buttonContainerStyle}> 
+       <Button style={buttonStyle} inverted>Explore</Button>
+       <Button style={buttonStyle} inverted>Login</Button>
+      </div>
+    )
+  }
+ 
+  return (
+    <div style={style}>
+      <Header/>
+      <div style={ContainerStyle}>
+        <img style={imageStyle} src='/logo-and-title.png'></img>
+        <Buttons/>
+      </div>
+    </div>
+  )
+}
+
+const style = {
+  backgroundColor:'#1ed760',
+  height:'100vh',
+  display:'flex',
+  flexDirection:'column',
+  justifyContent:'center'
+}
+
+const imageStyle = {
+  width:'300px',
+  height:'300px',
+  minHeight: '250px',
+  minWidth:'250px',
+  margin:'auto'
+}
+
+const ContainerStyle = {
+  width:'340px',
+  height:'300px',
+  minHeight: '250px',
+  minWidth:'250px',
+  margin:'auto',
+  textAlign:'center'
+}
+
+const headerContainer ={
+  position:'fixed',
+  top:0,
+  width:'100vw',
+  padding:'1.5rem'
 }
