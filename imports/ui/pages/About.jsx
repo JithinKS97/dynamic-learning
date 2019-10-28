138186dca104103467d4f679ea5b91c78865e6d6
JithinKS97 <jithinks112@gmail.com>
2019-10-28 14:19:21 +0530

    responsiveness correction

--- imports/ui/pages/About.jsx
+++ imports/ui/pages/About.jsx
@@ -44,7 +44,7 @@ const Description = () => (
   <div className="about__description">
     <h1 style={{ marginTop: "0.8rem" }}> About Dynamic Learning </h1>
     <h3 style={{ marginTop: "0rem" }}> What is Dynamic Learning? </h3>
-    <div style={{ width: "60%" }}>
+    <div className='about__description-paragraphs'>
       In the modern day and age where technology is such a prominent and
       important tool, STEM education has a lack of interactive and visual
       learning for interested students. Dynamic Learning is an application that
@@ -58,7 +58,7 @@ const Description = () => (
     </div>
     <br />
     <h4 style={{ marginTop: "0rem" }}> Workbooks </h4>
-    <div style={{ width: "60%" }}>
+    <div className='about__description-paragraphs'>
       Workbooks is a system in which slides can be created with embedded
       simulations made with p5.js. There is an interactive canvas which can be
       drawn on. The canvas can also include textboxes and descriptions of other
@@ -67,7 +67,7 @@ const Description = () => (
     </div>
     <br />
     <h4 style={{ marginTop: "0rem" }}> Lessons </h4>
-    <div style={{ width: "60%" }}>
+    <div className='about__description-paragraphs'>
       In a similar fashion to workbooks, lessons are a set of slides that can be
       used by teachers in the classroom. Lessons, as opposed to workbooks, use
       the idea of pairing videos with simulations. The lefthand side contains a
@@ -76,7 +76,7 @@ const Description = () => (
     </div>
     <br />
     <h4 style={{ marginTop: "0rem" }}> Classes </h4>
-    <div style={{ width: "60%" }}>
+    <div className='about__description-paragraphs'>
       Classes can be created by teachers and a unique class code will be created
       along with the class. Teachers can give these codes to students and they
       can add the classes to their own account. From the classes page, students
@@ -86,7 +86,7 @@ const Description = () => (
     <br />
     <h1 style={{ marginTop: "0rem" }}> For Developers </h1>
     <h4 style={{ marginTop: "0rem" }}> How to make simulations </h4>
-    <div style={{ width: "60%" }}>
+    <div className='about__description-paragraphs'>
       If you are interested in making simulations, feel free to create an
       account and look at some of the existing requests for simulations! In
       order to create simulations that work with Dynamic Learning, it is
