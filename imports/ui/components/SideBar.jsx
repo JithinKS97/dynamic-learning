f06f7337131f648d3fbc3bc8fafe2ed405336c50
JithinKS97 <jithinks112@gmail.com>
2019-11-17 15:14:09 +0530

    debigging udefined issur

--- imports/ui/components/SideBar.jsx
+++ imports/ui/components/SideBar.jsx
@@ -23,7 +23,7 @@ export default class SideBar extends React.Component {
           user: Meteor.user().username,
         });
       }
-      if (Meteor.user() && Meteor.user().services) {
+      if (Meteor.user().services) {
         console.log(Meteor.user().services);
         if (Meteor.user().services.github) {
           Meteor.call('setUsername', Meteor.user()._id, Meteor.user().services.github.username);
